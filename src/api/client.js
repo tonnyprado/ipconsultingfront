96b9c9384d08d399fd203b0d19edb728992268ff
tonyprado <tonnypradog@hotmail.com>
2025-11-20 23:52:40 -0600

    chore: fix client importmeta

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL  || "http://localhost:8080",
   timeout: 10000,
 });
 
